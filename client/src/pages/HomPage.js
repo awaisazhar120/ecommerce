858d55a3ef9e336adfba965796317edbdc4b6941
awais.azhar <awais.azhar@ibex.co>
2023-12-15 19:13:16 +0500

    routing general concept

---
+++ client/src/pages/HomPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from './../components/Layout/Layout';
+
+const HomPage = () => {
+  return (
+    <div>
+        <Layout>
+            <h1>Home Page</h1>
+        </Layout>
+    </div>
+  )
+}
+
+export default HomPage
---
+++ client/src/pages/PageNotFound.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from './../components/Layout/Layout';
+
+const PageNotFound = () => {
+  return (
+    <div>
+        <Layout>
+            <h1>Page Not Found</h1>
+        </Layout>
+    </div>
+  )
+}
+
+export default PageNotFound
