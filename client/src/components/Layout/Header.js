e366d5504cd9a8da23ad92c4382a7fefc6175e31
awais.azhar <awais.azhar@ibex.co>
2023-12-29 20:03:02 +0500

    added

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -102,38 +102,3 @@ const Header = () => {
 
 export default Header;
 
-<li class="dropdown-item pl-0">
-                                        <i class="fa-solid fa-camera mx-2"></i>
-                                        <a class="" href="javascript:;">{{ $menu->title }}</a>
-                                        <div class="mega-menu p-3 position-absolute">
-                                            if ($menu->parents->childs && !empty($menu->parents->childs))
-                                            <div class="child-cats-mega-menu">
-                                                <h5>{{ $menu->parents->title }}</h5>
-                                                <ul class="list-unstyled d-inline-block">
-                                                    @foreach ($menu->parents->childs as $child)
-                                                    <li>
-                                                        <a href="{{ category_url($child->id) }}">
-                                                            {{ $child->title }}
-                                                        </a>
-                                                    </li>
-                                                    @endforeach
-                                                </ul>
-                                            </div>
-                                            @endif
-                                            @if ($menu->stores)
-                                            <div class="top-stores-mega-menu">
-                                                <h5>Top Stores</h5>
-                                                <ul class="list-unstyled d-inline-block">
-                                                    @foreach ($menu->stores as $store)
-                                                    <li>
-                                                        <a href="#">
-                                                            {{ $store->title }}
-                                                        </a>
-                                                    </li>
-                                                    @endforeach
-                                                </ul>
-                                            </div>
-                                            @endif
-                                            <a href="" class="d-inline-block"><img class="img-fluid h-100" src="https://ti.tradetracker.net/?c=34438&m=2146083&a=411945&r=&t=custom" alt=""></a>
-                                        </div>
-                                    </li>
