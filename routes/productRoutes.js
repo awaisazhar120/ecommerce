106a7f7e1e09b38e19b60c6550e0521ff14e9a4c
awais.azhar <awais.azhar@ibex.co>
2024-01-05 20:19:14 +0500

    updated

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
-import { createProductController } from "../controllers/productController.js";
+import formidable from "express-formidable";
+import { createProductController, updateProductController } from "../controllers/productController.js";
 
 const router = express.Router();
 
-router.post("/create-product", requireSignIn, isAdmin, createProductController);
+router.post("/create-product", requireSignIn, isAdmin, formidable(), createProductController);
+
+router.put("/update-product/:id", requireSignIn, isAdmin, formidable(), updateProductController);
+
 export default router;
--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-import express from 'express';
-import colors from 'colors';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import connectDB from './config/db.js';
-import authRouters from './routes/authRoute.js'
-import categoryRoutes from './routes/categoryRoutes.js'
-import productRoutes from './routes/productRoutes.js'
-import cors from 'cors';
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRouters from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
 dotenv.config();
 
 connectDB();
 
 const app = express();
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
-app.use('/api/v1/auth',authRouters);
-app.use('/api/v1',categoryRoutes);
-app.use('api/v1/',productRoutes);
+app.use("/api/v1/auth", authRouters);
+app.use("/api/v1", categoryRoutes);
+app.use("/api/v1", productRoutes);
 
-app.get('/',(req, res) => {
-    res.send("<h1>Sendign Rsponse</h1>")
+app.get("/", (req, res) => {
+  res.send("<h1>Sendign Rsponse</h1>");
 });
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Server runnig on port ${PORT}`.bgCyan.white)
-})
+  console.log(`Server runnig on port ${PORT}`.bgCyan.white);
+});
